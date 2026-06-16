6a55b5e0d714294b408d1567f8bb18ae7e2ef6c9
PedroDias0 <84391347+PedroDias0@users.noreply.github.com>
2026-06-16 16:42:25 +0100

    registro

---
+++ src/php/registar.php
@@ -0,0 +1,26 @@
+<?php
+include "ligacao.php";
+
+$nome = $_POST["nome"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+$documento_tipo = $_POST["documento_tipo"];
+$documento_numero = $_POST["documento_numero"];
+$nif = $_POST["nif"];
+
+$password_hash = password_hash($password, PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO utilizadores 
+(nome, email, password, documento_tipo, documento_numero, nif, perfil, estado)
+VALUES 
+('$nome', '$email', '$password_hash', '$documento_tipo', '$documento_numero', '$nif', 'atleta', 'ativo')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Registo efetuado com sucesso!";
+    echo "<br><a href='../login.html'>Ir para login</a>";
+} else {
+    echo "Erro ao registar: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
